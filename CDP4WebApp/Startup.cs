48a99616a2bc123e054c988caba0f1013d592977
samatrhea <s.gerene@rheagroup.com>
2019-07-26 21:57:50 +0200

    [Add] Blazor App - built on top of standard blazor template

---
+++ CDP4WebApp/Startup.cs
@@ -0,0 +1,40 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Startup.cs" company="RHEA System S.A.">
+//    
+//  Copyright (c) 2019 RHEA System S.A.
+//
+//  Author: Sam Gerené
+//
+//  This file is part of CDP4-Web-Application
+//
+//  CDP4-Web-Application is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License.
+//
+//  CDP4-Web-Application is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with CDP4-Web-Application. If not, see<https://www.gnu.org/licenses/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4WebApp
+{
+    using Microsoft.AspNetCore.Components.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
